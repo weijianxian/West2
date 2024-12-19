1e7e4d2fad6ab50fb62a204afb86a30134193228
柠檬味氨水 <33548986+weijianxian@users.noreply.github.com>
2024-12-19 23:15:41 +0800

    添加获取知乎话题下热门问题的功能，并实现问题去重

--- task2/zhihu.py
+++ task2/zhihu.py
@@ -8,10 +8,32 @@ from selenium.webdriver import Edge
 from selenium.webdriver.common.by import By
 
 
+class Answer:
+    def __init__(
+        self,
+        url: str,
+        content: str,
+    ):
+        self.url = url
+        self.content = content
+
+
 class Question:
-    def __init__(self, title, question_url):
-        self.title = title
-        self.question_url = question_url
+    title = ""
+    question_url = ""
+    answer_list: list[Answer] = []
+
+    def __init__(self, title: str, question_url: str):
+        self.title: str = title
+        self.question_url: str = question_url
+
+    def __hash__(self):
+        return hash((self.title, self.question_url))
+
+    def __eq__(self, other):
+        if isinstance(other, Question):
+            return self.title == other.title and self.question_url == other.question_url
+        return False
 
     def __str__(self):
         return f"{self.title},{self.question_url}"
@@ -48,6 +70,35 @@ class Zhihu:
 
         return hot_question_list
 
+    def get_topic_top_question(self, url) -> list[Question]:
+        self.driver.get("https://www.zhihu.com/topic/19554298/top-answers")
+
+        # 滚动到底部
+        for _ in range(5):
+            sleep(2)
+            self.driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);"
+            )
+
+        sleep(5)
+        questions_list = self.driver.find_elements(By.CLASS_NAME, "TopicFeedItem")
+        topic_top_question_set: set[Question] = set()
+
+        for question in questions_list:
+            content = question.find_element(By.CLASS_NAME, "ContentItem-title")
+
+            try:
+                meata_data = content.find_elements(By.TAG_NAME, "meta")
+                url = meata_data[0].get_attribute("content")
+                title = meata_data[1].get_attribute("content")
+                # 问题去重
+                topic_top_question_set.add(Question(title, url))
+
+            except:
+                continue
+
+        return list(topic_top_question_set)
+
 
 def main():
     driver = Edge()
@@ -70,9 +121,15 @@ def main():
     zhihu = Zhihu(driver, load(open("cookies")))
 
     # 获取热榜问题
-    hot_question_list = zhihu.get_hot()
+    hot_question_list = zhihu.get_topic_top_question(
+        url="https://www.zhihu.com/topic/19554298/top-answers"
+    )
     for question in hot_question_list:
         print(question)
+    # 获取热榜问题
+    # hot_question_list = zhihu.get_hot()
+    # for question in hot_question_list:
+    #     print(question)
 
 
 if __name__ == "__main__":
