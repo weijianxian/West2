192aea0054e7aeaeb95faf882608c71dac8e45ab
柠檬味氨水 <33548986+weijianxian@users.noreply.github.com>
2024-12-20 23:39:34 +0800

    重构知乎爬虫，优化回答获取逻辑，支持动态滚动加载和回答内容解析

--- task2/zhihu.py
+++ task2/zhihu.py
@@ -1,51 +1,73 @@
 # 爬取知乎热榜问题
 
 import os
-from json import dump, load
+from json import dump, dumps, load, loads
 from time import sleep
 
-from selenium.webdriver import Edge
+from selenium.webdriver import Edge, EdgeOptions
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class Answer:
-    def __init__(
-        self,
-        url: str,
-        content: str,
-    ):
-        self.url = url
+    id: str = ""
+    content: WebElement = None
+    raw_content: str = ""
+
+    def __init__(self, answer_id: str, content: WebElement):
+        self.id = answer_id
         self.content = content
+        self.raw_content = content.get_attribute("outerHTML")
+
+        print(self.raw_content)
+
+    def __str__(self):
+        return f"{self.id}"
+
+    def parse_content(self):
+        # TODO: 解析回答内容
+        pass
 
 
 class Question:
     title = ""
-    question_url = ""
+    url = ""
+    id = ""
+
     answer_list: list[Answer] = []
 
     def __init__(self, title: str, question_url: str):
         self.title: str = title
-        self.question_url: str = question_url
+        self.url: str = question_url
+        self.id: str = question_url.split("/")[-1]
 
+    # 重写hash和eq方法，用于问题去重
     def __hash__(self):
-        return hash((self.title, self.question_url))
+        return hash((self.title, self.url))
 
     def __eq__(self, other):
         if isinstance(other, Question):
-            return self.title == other.title and self.question_url == other.question_url
+            return self.title == other.title and self.url == other.url
         return False
 
+    # 重写str方法，用于打印问题
     def __str__(self):
-        return f"{self.title},{self.question_url}"
+        return f"{self.title},{self.id},{self.url}"
 
 
 class Zhihu:
     is_login = False
 
-    def __init__(self, driver: Edge):
+    def __init__(self, driver: Edge = Edge()):
         self.driver = driver
 
     def login(self, cookies: list = None):
+        """
+        登录知乎
+        :param cookies: 传入cookies登录
+        :return: None
+        """
+
         print("正在登录")
 
         self.driver.get("http://www.zhihu.com/signin")
@@ -58,35 +80,39 @@ class Zhihu:
                 self.driver.add_cookie(cookie)
             self.driver.refresh()
             self.is_login = True
-            return
 
         # 未传入则检测本地cookie文件
-        if os.path.exists("cookies"):
+        elif os.path.exists("cookies"):
             print("通过本地cookies登录")
             cookies = load(open("cookies"))
             for cookie in cookies:
                 self.driver.add_cookie(cookie)
             self.driver.refresh()
             self.is_login = True
-            return
 
         # 未检测到cookie文件则手动登录
-        print(f"{'='*20}\n请登录\n{'='*20}")
-        while self.driver.current_url != "https://www.zhihu.com/":
-            sleep(1)
-        # 保存cookies
-        dump(
-            [
-                {"name": cookie["name"], "value": cookie["value"]}
-                for cookie in self.driver.get_cookies()
-            ],
-            open("cookies", "w"),
-        )
-        self.driver.refresh()
-        self.is_login = True
-        return True
+        else:
+            print(f"{'='*20}\n请登录\n{'='*20}")
+            while self.driver.current_url != "https://www.zhihu.com/":
+                sleep(1)
+            # 保存cookies
+            dump(
+                [
+                    {"name": cookie["name"], "value": cookie["value"]}
+                    for cookie in self.driver.get_cookies()
+                ],
+                open("cookies", "w"),
+            )
+
+            print("保存cookies成功")
+            self.driver.refresh()
+            self.is_login = True
 
     def get_hot(self) -> list[Question]:
+        """
+        获取知乎热榜问题
+        :return: 热榜问题列表
+        """
         self.driver.get("https://www.zhihu.com/hot")
         sleep(5)
         questions = self.driver.find_elements(By.CLASS_NAME, "HotItem")
@@ -101,11 +127,18 @@ class Zhihu:
 
         return hot_question_list
 
-    def get_topic_top_question(self, url) -> list[Question]:
-        self.driver.get("https://www.zhihu.com/topic/19554298/top-answers")
+    def get_topic_top_question(self, url: str, scroll_times: int = 5) -> list[Question]:
+        """
+        获取知乎话题热榜问题
+        :param url: 话题url
+        :param scroll_times: 滚动次数
+        :return: 问题列表
+        """
 
-        # 滚动到底部
-        for _ in range(5):
+        self.driver.get(url)
+
+        # 滚动
+        for _ in range(scroll_times):
             sleep(2)
             self.driver.execute_script(
                 "window.scrollTo(0, document.body.scrollHeight);"
@@ -130,23 +163,69 @@ class Zhihu:
 
         return list(topic_top_question_set)
 
+    def get_answer(self, question: Question, scroll_times: int = 5) -> list[Answer]:
+        """
+        获取问题的回答
+        :param question: 问题
+        :param scroll_times: 滚动次数
+        :return: 回答列表
+        """
+        print(f"正在获取问题：{question}")
+        driver = self.driver
+        driver.get(question.url)
+
+        # 滚动
+        for _ in range(scroll_times):
+            sleep(2)
+            print("正在滚动答案页面")
+            self.driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);"
+            )
+
+        answer_list: list[WebElement] = driver.find_elements(
+            By.CLASS_NAME, "AnswerItem"
+        )
+
+        # 获取回答
+        for answer_webelement in answer_list:
+            # 获取回答元数据
+            asnwer_meta_data = loads(answer_webelement.get_attribute("data-zop"))
+            answer_id = asnwer_meta_data["itemId"]
+
+            question.answer_list.append(
+                Answer(
+                    answer_id,
+                    answer_webelement.find_element(By.CLASS_NAME, "RichText"),
+                )
+            )
+
 
 def main():
-    driver = Edge()
 
-    zhihu = Zhihu(driver)
-    zhihu.login()
+    # 创建Edge浏览器 & 初始化知乎客户端
+    options = EdgeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
 
-    # 获取热榜问题
-    hot_question_list = zhihu.get_topic_top_question(
-        url="https://www.zhihu.com/topic/19554298/top-answers"
+    client = Zhihu(driver=Edge(options=options))
+
+    client.driver.execute_cdp_cmd(
+        "Page.addScriptToEvaluateOnNewDocument",
+        {
+            "source": """
+                Object.defineProperty(navigator, 'webdriver', {
+                get: () => undefined
+                })
+            """
+        },
     )
-    for question in hot_question_list:
-        print(question)
-    # 获取热榜问题
-    # hot_question_list = zhihu.get_hot()
-    # for question in hot_question_list:
-    #     print(question)
+    client.login()
+
+    test_question = Question("test", "https://www.zhihu.com/question/26742539")
+
+    client.get_answer(test_question)
+
+    input("回车退出")
 
 
 if __name__ == "__main__":
